b25a0d86dd148b8976a558af2658171feebf51b4
Eric Wajnrajch <ericwaje03@gmail.com>
2021-11-17 23:29:38 -0300

    vista de producto

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Item = ({ img, price }) => {
+const Item = ({ img, price, id }) => {
     return (
         <div style={{ margin: '10px 20px', border: '2px solid black' }}>
-            <img width="200px" src={img} alt="producto" />
+            <Link to={`${id}`}>
+                <img width="200px" src={img} alt="producto" />
+            </Link>
             <p>${price}</p>
         </div>
     );
