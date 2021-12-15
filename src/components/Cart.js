b50edbc5a1f531d4b20935d5d9e99da38b984e84
Eric Wajnrajch <ericwaje03@gmail.com>
2021-12-15 18:36:59 -0300

    form en carrito

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -7,6 +7,16 @@ import Order from './Order';
 const Cart = () => {
     const { cart, total, deleteAll } = useContext(CartContext);
     const [goTicket, setGoTicket] = useState(false);
+    const [form, getForm] = useState({});
+
+    const llenarFormulario = (e) => {
+        const { name, value } = e.target;
+        getForm({
+            ...form,
+            [name]: value,
+        });
+    };
+    console.log(form);
 
     const date = new Date();
 
@@ -14,7 +24,7 @@ const Cart = () => {
         const db = getFirestore();
         const ref = collection(db, 'ticket');
         const newOrder = {
-            buyer: 'eric',
+            buyer: form,
             items: cart,
             date: date,
             total: total(),
@@ -37,8 +47,27 @@ const Cart = () => {
                     <div>
                         <button onClick={deleteAll}>Vaciar Carrito</button>
                     </div>
+                    <form style={{ margin: '15px 0px' }}>
+                        <input
+                            onChange={llenarFormulario}
+                            type="email"
+                            name="email"
+                            placeholder="email"
+                        />
+                        <input
+                            onChange={llenarFormulario}
+                            type="text"
+                            name="nombre"
+                            placeholder="nombre"
+                        />
+                    </form>
                     <div>
-                        <button onClick={finalizar}>Finalizar compra 🙌🏼</button>
+                        <button
+                            onClick={finalizar}
+                            disabled={form.email === '' || form.nombre === ''}
+                        >
+                            Finalizar compra 🙌🏼
+                        </button>
                     </div>
                 </>
             ) : (
