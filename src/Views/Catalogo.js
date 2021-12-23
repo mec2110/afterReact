55f7d36c8b797a9b8188feaf962052f13c26f0af
Eric Wajnrajch <ericwaje03@gmail.com>
2021-12-23 18:44:16 -0300

    borrando catalogo

--- src/Views/Catalogo.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import ItemListContainer from '../components/Item/ItemListContainer';
-
-const Catalogo = () => {
-    return <Routes></Routes>;
-};
-
-export default Catalogo;
